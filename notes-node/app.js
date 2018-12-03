221dedc22191f0450e605816f01a5f197dfef51c
Salecyclejonathon <43176386+Salecyclejonathon@users.noreply.github.com>
2018-12-03 19:51:23 +0000

    Added custom notes function and addition function

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,15 +2,21 @@ console.log('Starting app');
 
 const fs = require('fs'); // Allows us to write to a file
 const os = require('os');
+const notes = require('./notes.js'); // require file for notes.js in our directory
 
-var user = os.userInfo();
-console.log(user); // allows us to view user info
+var res = notes.addNote();
+var addRes = notes.add(3, 8);
+console.log(addRes);
+console.log(res);
+
+// var user = os.userInfo();
+// console.log(user); // allows us to view user info
 
 // FS module
-fs.appendFile('greetings.txt', 'Hello ' + user.username, function (err) { // Catch error if not available
-  if (err) {
-    console.log('unable to write to file!');
-  }
-});
+// fs.appendFile('greetings.txt', 'Hello ' + user.username + 'you are ' + notes.age, function (err) { // Catch error if not available
+//   if (err) {
+//     console.log('unable to write to file!');
+//   }
+// });
 
-console.log('successfully printed to file!');
+// console.log('successfully printed to file!');
---
+++ notes-node/notes.js
@@ -0,0 +1,10 @@
+console.log('starting notes.js');
+
+module.exports.addNote = () => { // create function
+  console.log('add Note');
+  return 'New note';
+};
+
+module.exports.add = (a, b) => {
+  return a + b;
+};
