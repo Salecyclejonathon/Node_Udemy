facaa4c525b039917e50b9e50dfa34cf61a8d3ae
Salecyclejonathon <43176386+Salecyclejonathon@users.noreply.github.com>
2018-12-03 20:27:15 +0000

    small change

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,6 +2,5 @@ console.log('Starting app');
 
 const fs = require('fs'); // Allows us to write to a file
 const _ = require('lodash'); // install a third party module
-// const os = require('os');
 
 const notes = require('./notes.js'); // require file for notes.js in our directory
