48b7fa00790e6277a9b8600a8cfb02522f58c640
Salecyclejonathon <43176386+Salecyclejonathon@users.noreply.github.com>
2018-12-03 19:12:52 +0000

    Added OS feature to access user info

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,8 +1,13 @@
 console.log('Starting app');
 
-const fs = require('fs');
+const fs = require('fs'); // Allows us to write to a file
+const os = require('os');
 
-fs.appendFile('greetings.txt', 'Hello World!', function (err) {
+var user = os.userInfo();
+console.log(user); // allows us to view user info
+
+// FS module
+fs.appendFile('greetings.txt', 'Hello ' + user.username, function (err) { // Catch error if not available
   if (err) {
     console.log('unable to write to file!');
   }
